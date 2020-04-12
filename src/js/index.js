247b50228afa2723f169ff76c08fd39a1aa1825b
flmngwllm <flmngwllm232@gmail.com>
2020-04-12 15:35:54 -0400

    installed axios

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+async function getResults(query) {
+    
+}
+
+getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,4 @@
+// const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+
+
+// Recipe.js  const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
