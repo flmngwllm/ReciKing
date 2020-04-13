a1d9aba0ef69d6fa29916c3ef7583313df8cdd1d
flmngwllm <flmngwllm232@gmail.com>
2020-04-13 13:45:41 -0400

    created a function in searchView to limit words to a single line

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -11,6 +11,21 @@ export const clearRes = () => {
 
 }
 
+const limitRecTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if(title.length > limit){
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit){
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0); 
+
+        return `${newTitle.join(' ')} ...` 
+    }
+     return title   
+}
+
 const renderRecip = recipe => {
  const markUp = `
  <li>
@@ -19,7 +34,7 @@ const renderRecip = recipe => {
          <img src="${recipe.image_url}" alt="${recipe.title}">
      </figure>
      <div class="results__data">
-         <h4 class="results__name">${recipe.title}</h4>
+         <h4 class="results__name">${limitRecTitle(recipe.title)}</h4>
          <p class="results__author">${recipe.publisher}</p>
      </div>
  </a>
