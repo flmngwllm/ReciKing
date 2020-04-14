87119bf3cb67cf2144a3e9c1e7ec72685b1c3f2f
flmngwllm <flmngwllm232@gmail.com>
2020-04-14 16:04:02 -0400

    fninished recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -25,21 +25,28 @@ const controlSearch = async () => {
         // Prepare Ui for results
         searchView.clearInput();
         searchView.clearRes();
-
+       
         renderLoad(elem.searchRes)
         // search for the recipres
+        try {
         await state.search.getResults()
 
         //Render results on Ui
         clearLoader();
         searchView.renderRes(state.search.results)
-    }
+        } catch (err) {
+            alert('Something went wrong searching')
+            clearLoader();
+        }
+    } 
+
 }
 
 elem.searchForm.addEventListener('submit', e =>{
     e.preventDefault();
     controlSearch()
 })
+
 
 elem.searchResPages.addEventListener('click', e => {
     const btn = e.target.closest('.btn-inline')
@@ -56,4 +63,37 @@ const r = new Recipe(46956);
 r.getRecipe()
 console.log(r)
 
+const controlRec = async () => {
+    // entire url hash from browser
+    const id = window.location.hash.replace('#', '');
+    console.log(id)
 
+    if(id){
+        // Prepare UI for changes
+
+        // Create new recipe object
+        state.recipe = new Recipe(id)
+
+
+        try{
+
+        // Get recipe data and parse ingredients
+        await state.recipe.getRecipe()
+            
+
+        state.recipe.parseIngredients()
+        // Calculate servings and time
+
+        state.recipe.calcTime()
+        state.recipe.calcServings()
+        // Render Recipe
+
+        console.log(state.recipe)
+        } catch (err){
+            alert('Error loading recipe')
+        }
+    }
+}
+// window.addEventListener('hashchange', controlRec)
+// window.addEventListener('load', controlRec)
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRec))
